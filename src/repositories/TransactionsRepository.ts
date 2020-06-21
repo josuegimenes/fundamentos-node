9273f74d9c3a2d2fa9b22219dcddf77f067a4753
Josué Gimenes <josuegalvares@gmail.com>
2020-06-21 13:20:05 -0300

    Desafio concluído

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,11 @@
+import { uuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 interface Balance {
   income: number;
@@ -14,15 +21,46 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((previous, current) => {
+      if (current.type === 'income') {
+        return previous + current.value;
+      }
+      return previous;
+    }, 0);
+
+    const outcome = this.transactions.reduce((previous, current) => {
+      if (current.type === 'outcome') {
+        return previous + current.value;
+      }
+      return previous;
+    }, 0);
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const { total } = this.getBalance();
+
+    if (total < value && type === 'outcome') {
+      throw new Error('Insufficient funds!');
+    }
+
+    const transaction = {
+      id: uuid(),
+      title,
+      value,
+      type,
+    };
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
